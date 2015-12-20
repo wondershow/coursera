bf0a9491ade788ba62ae9fa0ab0dc425f983428d
zhanglei4reg@gmail.com <zhanglei4reg@gmail.com>
2015-12-20 10:55:56 -0500

    86.67 / 100.00, test 7 and timing test 2 failed. Need to see how to solve them

--- princeton/algoritm2/2ndtime/homewok/src/hw4/src/BoggleSolver.java
+++ princeton/algoritm2/2ndtime/homewok/src/hw4/src/BoggleSolver.java
@@ -1,13 +1,14 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
-
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class BoggleSolver
 {
 	private Dictionary dict;
+	
+	boolean[][] calculated;
+	int[][] neighbors;
 	
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
@@ -21,15 +22,21 @@ public class BoggleSolver
     public Iterable<String> getAllValidWords(BoggleBoard board) {
     	Set<String> res = new HashSet<String>();
     	boolean[][] checked = new boolean[board.rows()][board.cols()];
+    	
+    	
     	for (int i = 0; i < board.rows(); i++)
-    		for (int j = 0; j < board.cols(); j++) 
+    		for (int j = 0; j < board.cols(); j++) {
+    			//System.out.println("start dfs from i = " + i + ", j = " + j);
     			dfs(board, res, checked, i, j, "");
+    		}
     	return res;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
+    	if (!this.dict.contains(word)) return 0;
+    	
     	int len = word.length();
     	if (len <= 2)	return 0;
     	else if (len <= 4)	return 1;
@@ -40,10 +47,15 @@ public class BoggleSolver
     }
     
     public static void main(String[] args) {
-    	In in = new In("/Users/leizhang/coursera/princeton/algoritm2/2ndtime/homewok/src/hw4/src/hw4/boggle/dictionary-algs4.txt");
+    	In in = new In("/Users/leizhang/coursera/princeton/algoritm2/2ndtime/homewok/src/hw4/src/hw4/boggle/dictionary-16q.txt");
         String[] dictionary = in.readAllStrings();
         BoggleSolver solver = new BoggleSolver(dictionary);
-        BoggleBoard board = new BoggleBoard("/Users/leizhang/coursera/princeton/algoritm2/2ndtime/homewok/src/hw4/src/hw4/boggle/board4x4.txt");
+        
+        BoggleBoard board = new BoggleBoard("/Users/leizhang/coursera/princeton/algoritm2/2ndtime/homewok/src/hw4/src/hw4/boggle/board-16q.txt");
+        
+       // System.out.println(board.getLetter(0, 0));
+       // System.out.println(board.getLetter(2, 2));
+        
         int score = 0;
         for (String word : solver.getAllValidWords(board))
         {
@@ -51,19 +63,27 @@ public class BoggleSolver
             score += solver.scoreOf(word);
         }
         StdOut.println("Score = " + score);
+        
+        int [][] res = solver.neighbors(4, 4, 3, 3);
+       
+        System.out.println();
     }
     
     private void dfs(BoggleBoard board, Set<String> res, boolean[][] checked, int i, int j, String prefix) {
     	prefix = prefix + board.getLetter(i, j);
-    	//prefix = prefix.replaceFirst("Q", "QU");
-    	if (!this.dict.hasPrefix(prefix)) return;
-    	if (this.dict.contains(prefix)) res.add(prefix);
+//    	prefix = prefix.replaceFirst("Q", "QU");
+//    	System.out.println(prefix);
+    	String replaced = prefix.replaceFirst("Q", "QU");
+    	if (!this.dict.hasPrefix(replaced)) return;
+    	if (this.dict.contains(replaced)) res.add(replaced);
     	checked[i][j] = true;
     	int[][] next = neighbors(board.cols(), board.rows(), i, j);
+    	if (next == null) return;
+    	//System.out.println("starting point i = " + i + " j = " + j + ", col = " + board.cols() + ", row = " + board.rows());
     	for (int p = 0; p < next.length; p++) {
-    		System.out.println(next[p][0] + "," + next[p][1]);
+    		//System.out.println("checking:" + next[p][0] + "," + next[p][1]);
     		if (!checked[next[p][0]][next[p][1]]) {
-    			System.out.println("row = " + next[p][0] + ", col = " + next[p][1]);
+    			//System.out.println("row = " + next[p][0] + ", col = " + next[p][1]);
     			dfs(board, res, checked, next[p][0], next[p][1], prefix);
     		}
     	}
@@ -74,21 +94,37 @@ public class BoggleSolver
     	int[][] res;
     	int b = 0;
     	
+    	if (cols == 1 && rows == 1) {
+    		return null;
+    	} if (cols == 1) {
+    		if (i == 0 || i == rows - 1) res = new int[1][2];
+    		else res = new int[2][2];
+    		if (i != 0) res[b++] = new int[] {i - 1, j};
+    		if (i != rows - 1) res[b++] = new int[] {i + 1, j};
+    		return res;
+    	} else if (rows == 1) {
+    		if (j == 0 || j == cols - 1) res = new int[1][2];
+    		else res = new int[2][2];
+    		if (j != 0) res[b++] = new int[] {i, j - 1};
+    		if (j != cols - 1) res[b++] = new int[] {i, j + 1};
+    		return res;
+    	}
+    	
     	if ( (i == 0 || i == rows - 1) && (j == 0 || j == cols - 1) )      
-    		res = new int[4][2];
+    		res = new int[3][2];
     	else if ( (i == 0 || i == rows - 1) || (j == 0 || j == cols - 1) )
-    		res = new int[6][2];
+    		res = new int[5][2];
     	else
     		res = new int[8][2];
     	
     	if (i - 1 >= 0)	res[b++] = new int[] { i - 1 , j};
-    	if (i + 1 <= rows - 1) res[b++] = new int[] { i + 1 , j};
+    	if (i + 1 < rows) res[b++] = new int[] { i + 1 , j};
     	if (j - 1 >= 0) res[b++] = new int[] { i, j - 1};
-    	if (j + 1 <= cols - 1) res[b++] = new int[] { i, j + 1};
+    	if (j + 1 < cols) res[b++] = new int[] { i, j + 1};
     	if (i - 1 >= 0 && j - 1 >= 0) res[b++] = new int[] { i - 1, j - 1};
-    	if (i - 1 >= 0 && j + 1 <= cols - 1) res[b++] = new int[] { i - 1, j + 1};
-    	if (i + 1 <= rows - 1 && j - 1 >= 0) res[b++] = new int[] { i + 1, j - 1};
-    	if (i + 1 <= rows - 1 && j + 1 <= cols - 1) res[b++] = new int[] { i + 1, j + 1};
+    	if (i - 1 >= 0 && j + 1 < cols) res[b++] = new int[] { i - 1, j + 1};
+    	if (i + 1 < rows && j - 1 >= 0) res[b++] = new int[] { i + 1, j - 1};
+    	if (i + 1 < rows && j + 1 < cols) res[b++] = new int[] { i + 1, j + 1};
     	
     	return res;
     }
