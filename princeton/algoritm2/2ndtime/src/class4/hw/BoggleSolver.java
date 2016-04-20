23d7dcba096cffdebac16280cead801075497f24
zhanglei4reg@gmail.com <zhanglei4reg@gmail.com>
2016-04-20 19:30:42 -0400

    HW4 91.67/100, TreeSet Solution. backtracking optimization done. The performance results are as follows:
    Memory:
    Test 1: memory with dictionary-algs4.txt (must be <= 2x reference
    solution).
      - memory of dictionary[]           = 494472 bytes
      - memory of student   BoggleSolver = 831344 bytes
      - memory of reference BoggleSolver = 5135408 bytes
      - student / reference              = 0.16
    ==> passed
    
    Test 2: memory with dictionary-shakespeare.txt (must be <= 2x reference
    solution).
      - memory of dictionary[]           = 1925392 bytes
      - memory of student   BoggleSolver = 3251848 bytes
      - memory of reference BoggleSolver = 17473608 bytes
      - student / reference              = 0.19
    ==> passed
    
    Test 3: memory with dictionary-yawl.txt (must be <= 2x reference
    solution).
      - memory of dictionary[]           = 22595968 bytes
      - memory of student   BoggleSolver = 37351720 bytes
      - memory of reference BoggleSolver = 179259112 bytes
      - student / reference              = 0.21
    ==> passed
    
    
    Timing:
    Test 1: timing constructor (must be <= 5x reference solution).
     *  dictionary-algs4.txt
          - student   solution time (in seconds): 0.02
          - reference solution time (in seconds): 0.01
          - ratio:                                1.62
    
    ==> passed
    
     *  dictionary-enable2k.txt
          - student   solution time (in seconds): 0.06
          - reference solution time (in seconds): 0.02
          - ratio:                                2.95
    
    ==> passed
    
     *  dictionary-yawl.txt
          - student   solution time (in seconds): 0.08
          - reference solution time (in seconds): 0.04
          - ratio:                                1.88
    
    ==> passed
    
     *  dictionary-zingarelli2005.txt
          - student   solution time (in seconds): 0.20
          - reference solution time (in seconds): 0.09
          - ratio:                                2.08
    
    ==> passed
    
    Test 2: timing getAllValidWords() for 5.0 seconds using
    dictionary-yawl.txt
            (must be <= 2x reference solution)
          - reference solution calls per second: 8104.91
          - student   solution calls per second: 477.95
          - reference / student ratio:           16.96
    
    => passed    student <= 10000x reference
    => passed    student <=    25x reference
    => FAILED    student <=    10x reference
    => FAILED    student <=     5x reference
    => FAILED    student <=     2x reference

--- princeton/algoritm2/2ndtime/src/class4/hw/BoggleSolver.java
+++ princeton/algoritm2/2ndtime/src/class4/hw/BoggleSolver.java
@@ -1,19 +1,33 @@
 package class4.hw;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class BoggleSolver
 {
-
+	private TreeSetBoggleSolver treeSolver; 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		
+		String path1 = "./src/class4/hw/files/dictionary-algs4.txt";
+		String path2 = "./src/class4/hw/files/board-q.txt";
+		In in = new In(path1);
+	    String[] dictionary = in.readAllStrings();
+	    BoggleSolver solver = new BoggleSolver(dictionary);
+	    BoggleBoard board = new BoggleBoard(path2);
+	    int score = 0;
+	    for (String word : solver.getAllValidWords(board))
+	    {
+	        StdOut.println(word);
+	        score += solver.scoreOf(word);
+	    }
+	    StdOut.println("Score = " + score);
 	}
 	
 	// Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary)
     {
-    	
+    	treeSolver = new TreeSetBoggleSolver(dictionary);
     }
     
     /***
@@ -25,13 +39,13 @@ public class BoggleSolver
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board)
     {
-    	
+    	return treeSolver.getAllValidWords(board);
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word)
     {
-    	
+    	return treeSolver.scoreOf(word);
     }
 }
--- princeton/algoritm2/2ndtime/src/class4/hw/TreeSetBoggleSolver.java
+++ princeton/algoritm2/2ndtime/src/class4/hw/TreeSetBoggleSolver.java
@@ -1,5 +1,6 @@
 package class4.hw;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.TreeSet;
 
@@ -15,6 +16,7 @@ public class TreeSetBoggleSolver
 		dict = new TreeSet<String>();
 		for (int i = 0; i < dictionary.length; i++)
 			dict.add(dictionary[i]);
+		//System.out.println(dict.ceiling("ESOTERIC "));
 	}
 	
 	/***
@@ -26,27 +28,77 @@ public class TreeSetBoggleSolver
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board)
     {
+    	if (board == null) throw new NullPointerException();
     	HashSet<String> set = new HashSet<String>();
-    	
     	int row = board.rows(), col = board.cols();
-    	
     	boolean[][] marked;
     	for (int i = 0; i < row; i++)
     		for (int j = 0; j < col; j++) 
     		{
     			marked = new boolean[board.rows()][board.cols()];
     			marked[i][j] = true;
-    			dfs(i, j, board, set, marked, "");
+    			dfs(i, j, board, set, marked, board.getLetter(i, j)+"");
     		}
-    	
+    	return set;
+    	//return new ArrayList();
     }
     
     private void dfs(int row, int col, BoggleBoard board, HashSet<String> set, boolean[][] marked, String pre) 
     {
     	int ROW = board.rows(), COL = board.cols();
     	String word = getWord(pre);
-    	if (dict.contains(word)&&word.length() >= 3) set.add(word);
-    	if (row > 0) { dfs(row - 1, col, board, set, marked, pre + board.getLetter(row - 1, col));}
+    	//System.out.println(pre);
+    	if (dict.contains(word) && word.length() >= 3) set.add(word);
+    	if (!containsPrefix(word)) return;
+    	
+    	if (row > 0 && !marked[row - 1][col]) 
+    	{ 
+    		marked[row - 1][col] = true;
+    		dfs(row - 1, col, board, set, marked, pre + board.getLetter(row - 1, col));
+    		marked[row - 1][col] = false;
+    	}
+    	if (col > 0 && !marked[row][col - 1]) 
+    	{ 
+    		marked[row][col - 1] = true;
+    		dfs(row, col - 1, board, set, marked, pre + board.getLetter(row, col - 1));
+    		marked[row][col - 1] = false;
+    	}
+    	if (row < ROW - 1 && !marked[row + 1][ col])  
+    	{ 
+    		marked[row + 1][col] = true;
+    		dfs(row + 1, col, board, set, marked, pre + board.getLetter(row + 1, col));
+    		marked[row + 1][col] = false;
+    	}
+    	if (col < COL - 1 && !marked[row][col + 1]) 
+    	{ 
+    		marked[row][col + 1] = true;
+    		dfs(row, col + 1, board, set, marked, pre + board.getLetter(row, col + 1));
+    		marked[row][col + 1] = false;
+    	}
+    	if (row > 0 && col > 0 && !marked[row - 1][col - 1] ) 
+    	{ 
+    		marked[row - 1][col - 1] = true;
+    		dfs(row - 1, col - 1, board, set, marked, pre + board.getLetter(row - 1, col - 1));
+    		marked[row - 1][col - 1] = false;
+    	}
+    	if (row < ROW - 1 && col < COL - 1 && !marked[row + 1][col + 1]) 
+    	{
+    		marked[row + 1][col + 1] = true;
+    		dfs(row + 1, col + 1, board, set, marked, pre + board.getLetter(row + 1, col + 1));
+    		marked[row + 1][col + 1] = false;
+    	}
+    	if (row > 0 && col < COL - 1 && !marked[row - 1][col + 1]) 
+    	{ 
+    		marked[row - 1][col + 1] = true;
+    		dfs(row - 1, col + 1, board, set, marked, pre + board.getLetter(row - 1, col + 1));
+    		marked[row - 1][col + 1] = false;
+    	}
+    	if (row < ROW - 1 && col > 0 && !marked[row + 1][col - 1]) 
+    	{ 
+    		marked[row + 1][ col - 1] = true;
+    		dfs(row + 1, col - 1, board, set, marked, pre + board.getLetter(row + 1, col - 1));
+    		marked[row + 1][ col - 1] = false;
+    	}
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
@@ -54,12 +106,36 @@ public class TreeSetBoggleSolver
     public int scoreOf(String word)
     {
     	if (word == null) throw new NullPointerException();
-    	
+    	if (!dict.contains(word)) return 0;
+    	//String realWord = getWord(word);
+    	int len = word.length();
+    	if (len <= 2) return 0;
+    	if (len <= 4) return 1;
+    	if (len <= 5) return 2;
+    	if (len <= 6) return 3;
+    	if (len <= 7) return 5;
+    	return 11;
     }
     
     //replace Q with Qu
     private String getWord(String word) 
     {
     	return word.replace("Q", "QU");
+    }
+    
+    public static void main(String[] args)
+    {
+    	for (int i = 0; i < 54; i ++)
+    	{
+    		System.out.println(i +  ":"  + (double)Math.round((double)i*40/54) / 2     );
+    	}
+    }
+    
+    // returns if the treeset contains elements that has a given prefix
+    private boolean containsPrefix(String pre) 
+    {
+    	String next = dict.ceiling(pre + " ");
+    	if (next != null && next.startsWith(pre)) return true;
+    	return false;
     }
 }
