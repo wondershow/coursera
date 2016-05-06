b664cfe90f86d68ecc9fb00f01d678c90b854421
zhanglei4reg@gmail.com <zhanglei4reg@gmail.com>
2016-05-06 17:18:24 -0400

    HW5 not finished yet.

--- princeton/algoritm2/2ndtime/src/class5/hw/BurrowsWheeler.java
+++ princeton/algoritm2/2ndtime/src/class5/hw/BurrowsWheeler.java
@@ -1,6 +1,126 @@
 package class5.hw;
 
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 public class BurrowsWheeler
 {
+	private static int R = 256;
+	// apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
+    public static void encode()
+    {
+    	String s = BinaryStdIn.readString();
+    	int N = s.length();
+    	
+    	CircularSuffixArray csa = new CircularSuffixArray(s);
+    	
+    	int first = -1;
+    	for (int i = 0; i < N; i++)
+    		if (csa.index(i) == 0) { first = i; break; }
+    	
+    	BinaryStdOut.write(first, 32);
+    	
+    	for (int i = 0; i < N; i++)
+    	{
+    		int shift = csa.index(i);
+    		BinaryStdOut.write( s.charAt( (N-1 + shift) % N), 8);
+    	}
+    	
+    	BinaryStdOut.flush();
+        BinaryStdOut.close();
+    }
 
+    // apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
+    public static void decode()
+    {
+    	int first = BinaryStdIn.readInt(32);
+    	
+    	//System.out.println("first = " + first);
+    	
+    	String s = BinaryStdIn.readString();
+    	int N = s.length();
+    	
+    	int[] t  = new int[N];
+    	for (int i = 0; i < N; i++) t[i] = s.charAt(i);
+    	int[] next = new int[N];
+    	Arrays.fill(next, -1);
+    	
+    	
+    	int[] firstChars = sortArr(t, N, s);
+    	
+    	for (int i = 0; i < N; i++)
+    	{
+    		char c = (char)firstChars[i];
+    		int j = 0;
+    		for (j = 0; j < N; j++)
+    		{
+    			if (t[j] == c) 
+    			{
+    				t[j] = -1;
+    				next[i] = j; 
+    				break; 
+    			}
+    		}
+    		//System.out.println(c + " : " +  "j = " + j + ", t[j] = " + t[j]);
+    	}
+    	
+    	/*
+    	for (int i =0; i < N; i++)
+    		System.out.println(i + " : " + next[i]);*/
+    	
+    	int index = first;
+    	int len = 1;
+    	while (true) 
+    	{
+    		char c = (char)firstChars[index];
+    		BinaryStdOut.write(c, 8);
+    		index = next[index];
+    		if (len == N ) break;
+    		len++;
+    	}
+    	
+    	//for (int i = 0; i < N; i++)
+    		//System.out.println(next[i]);
+    	BinaryStdOut.flush();
+        BinaryStdOut.close();
+    }
+    
+    private static int[] sortArr(int[] t, int N, String s)
+    {
+    	int[] res = new int[N];
+    	
+    	int[] count = new int[R+1];
+    	
+    	for (int i = 0; i < N; i++)
+    		count[s.charAt(i) + 1]++;
+    	for (int i = 1; i <= R; i++)
+    		count[i] += count[i-1];
+    	for (int i = 0; i <N; i++)
+    	{
+    		char c = (char)t[i];
+    		res[count[(int)c]++] = c;
+    	}
+    	/*
+    	for (int i = 0; i < N; i++)
+    		System.out.println(res[i]);*/
+    	
+    	return res;
+    }
+
+    // if args[0] is '-', apply Burrows-Wheeler encoding
+    // if args[0] is '+', apply Burrows-Wheeler decoding
+    public static void main(String[] args)
+    {
+    	if (args.length < 1) throw new java.lang.NullPointerException();
+		if (args[0].equals("-"))
+			encode();
+		else
+		if (args[0].equals("+"))
+			decode();
+		else
+			throw new java.lang.IllegalArgumentException();
+	}
+    	
 }
--- princeton/algoritm2/2ndtime/src/class5/hw/CircularSuffixArray.java
+++ princeton/algoritm2/2ndtime/src/class5/hw/CircularSuffixArray.java
@@ -2,5 +2,146 @@ package class5.hw;
 
 public class CircularSuffixArray
 {
-
+	private int N; 
+	private int[] index;
+	
+	private static final int R = 256;
+	private static final int CUTOFF = 5;
+	//private final String csstr;
+	private final char[] strArr;
+	/*
+	private class MySubStr 
+	{
+		public int start, cur;
+		MySubStr(int s, int d) 
+		{
+			start = s;
+			cur = d;
+		}
+	}*/
+	
+	public CircularSuffixArray(String s)  // circular suffix array of s
+	{
+		if (s == null) throw new java.lang.NullPointerException();
+		N = s.length();
+		
+		index = new int[N];
+		strArr = new char[2*N];
+		int i;
+		
+		for (i = 0; i < N; i++) 
+		{
+			index[i] = i;
+			
+			strArr[i] = s.charAt(i); 
+			strArr[i+N] = s.charAt(i);
+			
+		}
+		
+		quick3sort(index, 0, N-1, 0);
+		//LSDSort(suffixChars, subStrs); 
+	}
+	
+	private int charAt(int offset, int d) 
+	{
+		return strArr[offset + d];
+	}
+	
+	private void quick3sort(int[] rank, int lo, int hi, int d)
+	{
+		//System.out.println("hi = " + hi + ", lo = " + lo + " d = " + d);
+		if (d >= N) return;
+		if ( hi <= lo + CUTOFF ) 
+		{
+			insertion(rank, lo, hi, d);
+			return;
+		}
+		
+		int p = charAt(rank[lo], d);
+		
+		int lt = lo, gt = hi, i = lo +1;
+		
+		while (i <= gt)
+		{
+			int c = charAt(rank[i], d);
+			if (c > p) { exch(rank, i, gt--); }
+			else if (c < p) {exch(rank, i++, lt++); }
+			else i++;
+		}
+		
+		quick3sort(rank, lo, lt-1, d);
+		quick3sort(rank, lt, gt, d+1);
+		quick3sort(rank, gt+1, hi, d);
+	}
+	
+	private void exch(int[] rank, int i, int j)
+	{
+		int tmp = rank[i];
+		rank[i] = rank[j];
+		rank[j] = tmp;
+	}
+	
+	private void insertion(int[] rank, int lo, int hi, int d) 
+	{
+		
+		for (int i = lo; i < hi; i++)
+			for (int j = i; j > lo; j--)
+			{
+				if ( compare(rank, j, j-1, d) < 0 )
+					exch(rank, j, j-1);
+				else
+					break;
+			}
+		
+	}
+	
+	private int compare(int[] rank, int i, int j, int d)
+	{
+		while (d<=N-1 && charAt(rank[i], d) == charAt(rank[j], d)) d++;
+		return charAt(rank[i], d) - charAt(rank[j], d);
+	}
+	
+	/*
+	private void LSDSort(char[] sfxChar, MySubStr[] subStrArr) 
+	{
+		int i;
+		
+		for (int d = N - 1; d >= 0; d--)
+		{
+			int[] count = new int[R+1];
+			for (i = 0; i < N; i++)
+				count[charAt(sfxChar, subStrArr[i]) + 1]++; 
+			
+			for (i = 1; i < R+1; i++)
+				count[i] += count[i-1];
+			
+			for (i = 0; i < N; i++)
+				aux[count[charAt(sfxChar, subStrArr[i])]++] = subStrArr[i];
+			
+			for (i = 0; i < N; i++) 
+			{
+				subStrArr[i] = aux[i];
+				aux[i].cur--;
+			}
+			//System.out.println("d = " + d);
+		}
+	}*/
+	
+	public int length()                   // length of s
+	{
+		return N;
+	}
+	
+	public int index(int i)               // returns index of ith sorted suffix
+	{
+		if (i < 0 || i >= N) throw new java.lang.IndexOutOfBoundsException();
+		return index[i];
+	}
+	
+	public static void main(String[] args)// unit testing of the methods (optional)
+	{
+		CircularSuffixArray csa = new CircularSuffixArray("ABRACADABRA!");
+		for (int i = 0; i < csa.length(); i++)
+			System.out.println(csa.index(i));
+	}
 }
